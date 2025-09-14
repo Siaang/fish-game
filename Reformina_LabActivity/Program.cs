a432588230808cc73c8cd9998379e30d368a4622
Sia <122100161+Siaang@users.noreply.github.com>
2025-09-14 13:21:05 +0800

    initial setup

---
+++ Reformina_LabActivity/Program.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Raylib.InitWindow(800, 600, "Insaniquarium Clone");
+        Raylib.SetTargetFPS(60);
+    }
+}
